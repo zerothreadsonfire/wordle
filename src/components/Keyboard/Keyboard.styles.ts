18091f33e12d01642f0c8d9007a6f34464d3a97b
satyamKumar <satyamkumar2302@gmail.com>
2022-04-13 00:09:44 +0530

    commit after work

--- src/components/Keyboard/Keyboard.styles.ts
+++ src/components/Keyboard/Keyboard.styles.ts
@@ -2,7 +2,16 @@ import { StyleSheet } from "react-native";
 
 const keyboardStyles = StyleSheet.create({
   container: {
-    
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 6,
+    flexWrap: "wrap",
+  },
+  key: {
+    width: 40,
+    height: 20,
+    margin: 2,
   }
 });
 
--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -3,10 +3,17 @@ import { Text, View, Image, Button, TouchableOpacity } from 'react-native';
 
 import keyboardStyles from "./Keyboard.styles";
 
+const APLHABETS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
 const Keyboard = () => {
   return (
     <View style={keyboardStyles.container}>
-      
+      {APLHABETS.map((value, index) => {
+        return (
+          <View key={index} style={keyboardStyles.key}>
+            <Button title={value} onPress={() => {}}></Button>
+          </View>) 
+      })}
     </View>
   )
 }
